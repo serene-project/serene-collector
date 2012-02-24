1a06fd07e4da8296a62760102bab97287b968a2d
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2012-02-24 18:55:09 +0100

    created module directory

--- src/main/java/net/sereneproject/collector/service/AnalyzerService.java
+++
@@ -1,9 +0,0 @@
-package net.sereneproject.collector.service;
-
-import net.sereneproject.collector.domain.ProbeValue;
-
-public interface AnalyzerService {
-
-    void analyze(ProbeValue probeValue);
-
-}
